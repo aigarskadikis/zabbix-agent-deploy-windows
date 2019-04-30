954007a708b878e9c2a2215c0a08d98a7ede220d
catonrug <aigars.kadikis@gmail.com>
2019-04-30 13:53:51 +0300

    print version of installed agent

--- check-agent-installed.cmd
+++ check-agent-installed.cmd
@@ -17,10 +17,37 @@ echo .. already running
 
 reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Zabbix Agent" > nul 2>&1
 if !errorlevel!==0 (
+
+if not exist "%~dp0sed.exe" (
 echo.
 echo .. location and config:
-if exist "%~dp0sed.exe" reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Zabbix Agent" /v ImagePath | findstr "ImagePath" | sed "s/^.*REG_EXPAND_SZ[ \t]*//"
-if not exist "%~dp0sed.exe" reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Zabbix Agent" /v ImagePath | findstr "ImagePath"
+reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Zabbix Agent" /v ImagePath | findstr "ImagePath"
+)
+
+rem if sed utility is placed in dir then run custom analisys
+if exist "%~dp0sed.exe" (
+for /f "tokens=*" %%v in ('^
+reg query "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\Zabbix Agent" /v ImagePath ^|
+findstr "ImagePath" ^|
+sed "s/^.*REG_EXPAND_SZ[ \t]*//"') do (
+
+rem extract where exactly the binary is installed
+for /f "tokens=*" %%b in ('^
+echo %%v ^|
+grep -E -o ".:.*exe"') do (
+echo.
+echo binary location:
+echo %%b
+echo.
+echo agent version:
+%%b -V | grep -E -o "[0-9]+\.[0-9]+\.[0-9]+"
+)
+
+
+)
+
+)
+
 )
 
 ) else echo .. not installed as service
